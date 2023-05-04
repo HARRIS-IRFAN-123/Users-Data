9f268cfc331c06f65ae3ab2502ac61af3a6d4e9b
Your Name <you@example.com>
2023-05-04 17:34:32 +0500

    added react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React,{useState}from 'react'
 import './App.css';
+import UsersTable from './components/UsersTable';
 
-function App() {
+function App() { 
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const[list,setList] = useState([]);
+  const handleSubmit= (e) => {
+    e.preventDefault();
+    console.log(name,email);
+    const data = {name,email}
+    if(name && email){
+      setList((ls) => [...ls,data]);
+      setName("")
+      setEmail("")
+      
+    }
+    fetch("https://react-client-59432-default-rtdb.firebaseio.com/users.json", {
+      method : 'POST',
+      headers:{
+        'Content-Type' : "application/json"
+      },
+      body : JSON.stringify(data)
+    })
+  }
+  const users = [
+    {name : "avg", email : "avg@gmail.com"}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To Our Users Form.</h1>
+      <UsersTable users = {users}/>
+      <form onSubmit={handleSubmit}>
+     <input name='name' placeholder='Name' value={name} onChange={(e)=> setName(e.target.value)} />
+     <input name='email' placeholder='Email' value={email} onChange={(e)=> setEmail(e.target.value)} />
+     <button>ADD USER</button>
+     </form>
     </div>
   );
 }
---
+++ src/components/UsersTable.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function UserTable({ users }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user, index) => (
+          <tr key={index}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default UserTable;
